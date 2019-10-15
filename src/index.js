068cca276fc844bc6157a31bcb046feb4b40c273
Gustavo <Gustavomgu>
2019-10-15 11:11:57 -0300

    chore : Using route params

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ const express = require('express');
 
 const server = express();
 
-server.get('/teste', (request,response) => {
-  return response.status(200).json({ message : 'Funcionou!'});
+//Usando route params
+
+server.get('/users/:id', (request,response) => {
+  const { id } = request.params;
+  // As duas linhas tem o mesmo objetivo, porem a linha acima usa desestruturação
+  //const id = request.params.id
+
+  return response.json({ message : `O id do usuário é : ${id} ` });
 });
+
+// Usando query params 
+
+// server.get('/teste', (request,response) => {
+  
+//   const nome = request.query.nome; 
+
+//   return response.status(200).json({ message : `Hello ${nome}`});
+// });
 
 server.listen(3000);
 
